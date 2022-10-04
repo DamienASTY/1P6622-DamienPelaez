8924440a3adddca9df694f6e8a0a5536086772fe
200051 <200051@PCI42101>
2022-10-04 13:41:33 +0200

    Ajoutez des fichiers projet.

---
+++ 1P6622-DamienPelaez/Feux.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _1P6622_DamienPelaez
+{
+    class Feux
+    {
+        private int id;
+        private string color;
+        private bool on = false;
+
+        public Feux(int id, string color)
+        {
+            this.color = color;
+            this.id = id;
+        }
+
+        public void setColor(string color)
+        {
+            this.color = color;
+
+        }
+
+        public string getColor()
+        {
+            return this.color;
+        }
+
+        public int getId()
+        {
+            return this.id;
+        }
+
+        public void clignoter()
+        {
+            this.on = !this.on;
+            string status = "";
+            if(this.on)
+            {
+                status = "allumé";
+            }
+            else
+            {
+                status = "éteint";
+            }
+            Console.WriteLine($"Le feux {this.id} est {status}");
+        }
+
+        public override string ToString()
+        {
+            return $"Le feux de signalisation {this.id} est {this.color}";
+        }
+    }
+}
---
+++ 1P6622-DamienPelaez/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _1P6622_DamienPelaez
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Etats des feux");
+            Feux feu = new Feux(id: 1001, color: "Rouge");
+            Console.WriteLine(feu);
+            Feux feu2 = new Feux(id: 007, "Vert");
+            Console.WriteLine(feu2);
+            Console.WriteLine("Changement d'états");
+            feu.setColor(color: "Orange");
+            Console.WriteLine(feu);
+            feu.setColor(color: "Vert");
+            Console.WriteLine(feu);
+            feu.setColor(color: "Rouge");
+            Console.WriteLine(feu);
+            feu.setColor(color: "Orange");
+            Console.WriteLine(feu);
+            feu.setColor(color: "Vert");
+            Console.WriteLine(feu);
+            Console.WriteLine("Feu Clignotant");
+            Random rand = new Random();
+            for(int i = 0; i < 5; i++)
+            {
+                feu2.clignoter();
+            }
+        }
+    }
+}
